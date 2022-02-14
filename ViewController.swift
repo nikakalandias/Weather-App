057569838e4d8b3ec001d4ecdbbdea3fa80b3270
nika <nikakalandias@gmail.com>
2022-02-14 08:43:10 +0300

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weather-App
+//
+//  Created by Nika Kalandia on 14.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor(patternImage: UIImage(named: "weatherbackground.png")!)
+    }
+
+
+}
+
