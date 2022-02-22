4925ae2c1d139b3377228ceb25bf28bd173fc43e
nika <nikakalandias@gmail.com>
2022-02-22 14:41:43 +0300

    httpResponse, Enum

--- JSONDownloader.swift
+++ JSONDownloader.swift
@@ -25,10 +25,28 @@ class JSONDownloader {
     func jsonTask(with request : URLRequest , completionHandler completion : @escaping (JSON?, OpenWeatherMapError?) -> Void) -> URLSessionDataTask {
         let task = session.dataTask(with: request) { data, response, error in
             guard let httpResponse = response as? HTTPURLResponse else {
-                
-                completion(nil, OpenWeatherMapError.RequestError)
+                completion(nil, .RequestError)
                 return
             }
+            if httpResponse.statusCode == 200 {
+                if let data = data {
+                    
+                    do {
+                        let json = try JSONSerialization.jsonObject(with: data, options: []) as? JSON
+                        completion(json, nil)
+                    }catch {
+                        completion(nil, .JSONParsingError)
+                    }
+                    
+                }else {
+                    // Data'da sorun varsa, veya elde edilemediyse.
+                    completion(nil, .InvalidData)
+                }
+            } else {
+                // Hata meydana geldi.
+                completion(nil, .ResponseUnsuccesful)
+            }
+            
             
         }
         return task
@@ -37,4 +55,7 @@ class JSONDownloader {
 
 enum OpenWeatherMapError {
     case RequestError
+    case ResponseUnsuccesful
+    case InvalidData
+    case JSONParsingError
 }
